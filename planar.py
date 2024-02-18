8482cf77a96359babff5821d2b0cc0cd293537d3
Martin Szewieczek <e1627784@student.tuwien.ac.at>
2024-02-18 19:03:45 +0100

    initialization; forward propagation; cost

--- planar.py
+++ planar.py
@@ -21,7 +21,97 @@ def print_data_shape():
     print ('The shape of Y is: ' + str(shape_Y))
     print ('I have m = %d training examples!' % (m))
 
+def layer_sizes(X, Y):
+    """
+    Arguments:
+    X -- input dataset of shape (input size, number of examples)
+    Y -- labels of shape (output size, number of examples)
+    
+    Returns:
+    n_x -- the size of the input layer
+    n_h -- the size of the hidden layer
+    n_y -- the size of the output layer
+    """
+    n_x = X.shape[0]
+    n_h = 4
+    n_y = Y.shape[0]
+    return (n_x, n_h, n_y)
+
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+    
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """    
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def forward_propagation(X, parameters):
+    """
+    Argument:
+    X -- input data of size (n_x, m)
+    parameters -- python dictionary containing your parameters (output of initialization function)
+    
+    Returns:
+    A2 -- The sigmoid output of the second activation
+    cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
+    """
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    # Implement Forward Propagation to calculate A2 (probabilities)
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+    
+    assert(A2.shape == (1, X.shape[1]))
+    
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+    return A2, cache
+
+def compute_cost(A2, Y):
+    """
+    Computes the cross-entropy cost given in equation (13)
+    
+    Arguments:
+    A2 -- The sigmoid output of the second activation, of shape (1, number of examples)
+    Y -- "true" labels vector of shape (1, number of examples)
+
+    Returns:
+    cost -- cross-entropy cost given equation (13)
+    
+    """
+    m = Y.shape[1] # number of examples
+
+    # Compute the cross-entropy cost
+    logprobs = np.multiply(np.log(A2),Y)
+    cost = - np.sum(logprobs + np.multiply((1-Y), np.log(1-A2)))/m
+    cost = float(np.squeeze(cost))  # makes sure cost is the dimension we expect. 
+                                    # E.g., turns [[17]] into 17 
+    return cost
+
 
 X, Y = load_planar_dataset()
-visualize_data()
-print_data_shape()
